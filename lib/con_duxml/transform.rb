03b5bc2e14ab16410fddb2496a4757bf2df77d1d
ludocracy <bansuki@gmail.com>
2016-08-22 19:31:35 -0500

    got most transforms working

--- lib/con_duxml/transform.rb
+++ lib/con_duxml/transform.rb
@@ -1,11 +1,15 @@
 # Copyright (c) 2016 Freescale Semiconductor Inc.
 require 'duxml'
 require_relative 'duxml_ext/transform_class'
+require_relative 'transform/transform_private'
 include Duxml
 
-# helper methods for converting transform elements into code
+# All public methods can be invoked by a transform element; please hide methods you don't want users to invoke in Private
 module Transform
   include Observable
+  include Private
+
+  # most recent XML node from which content has been taken; for constructing relative paths
   @source
 
   attr_reader :source
@@ -16,6 +20,8 @@ module Transform
     @output.history #TODO find transform whose outputs include given node
   end
 
+  # @param node [Element] XML node from transform output
+  # @return [Element] XML node that contains instructions for transform used to create given @param node
   def find_transform(node)
     find_xform_event(node).instructions
   end
@@ -26,68 +32,25 @@ module Transform
     find_xform_event(node).input
   end
 
-  # @param xform [Element] transform element
-  # @param _source [Element] XML xform containing content to be transformed
-  # @return [Array[Element]] transformed content; always an array of nodes, even if just one
-  def activate(xform, _source)
-    @source = _source
-    get_sources(xform).collect do |src|
-      args = get_args(xform, src)
-      output = get_method(xform).call(*args)
-      changed
-      notify_observers(:Transform, xform, src, output)
-      changed false
-      output
+  # @param path [String] path to node from @source e.g. 'child/grandchild'; target is expected to return ONE child
+  #   containing TEXT only
+  # @return [String] content of target node
+  def content(path)
+    @source.locate(add_name_space_prefix path).first.text
+  end
+
+  # @param *args [*several_variants] see Duxml::Element#new; the only difference here is that this method has access to the content source
+  #   so the arguments can pass in the needed data
+  # @return [Element] new element to replace old one
+  def element(*args)
+    Element.new(*args)
+  end
+
+  # @param path [String] path to node from @source
+  # @return [Element] a deep copy of the target(s)
+  def copy(path)
+    @source.locate(add_name_space_prefix path).collect do |node|
+      node.dclone
     end
-  end
-
-  # @param xform [Element] transform element
-  # @return [Array] array of elements that match @target, which must be a '/'-separated string
-  #   if transform element has any children that may need the same source target, target_stack.last remains
-  #   if transform is a leaf, target_stack is popped
-  def get_sources(xform)
-    if xform[:source]
-      source.locate add_name_space_prefix xform[:source]
-    else
-      [source]
-    end
-  end
-
-  # @param xform [Element] transform element
-  # @return [Method] resolves reference to actual transform method
-  def get_method(xform)
-    words = xform.name.split(':').reverse
-    method_name = words[0].to_sym
-    maudule = self
-    maudule = Module.const_get(words[1].constantize) if words[1] and Module.const_defined?(words[1].constantize)
-    maudule.method(method_name)
-  end
-
-  # @param xform [Element] transform element
-  # @param subj [Element] source XML Element
-  # @return [Array[String, Element]] string returned by self[:args] is separated by ';' into correctly formatted argument values for transform method
-  def get_args(xform, subj)
-    args = xform.attributes.keys.sort.collect do |attr|
-      if attr.to_s.match(/arg[0-9]/)
-        if xform[attr].include?(',')
-          xform[attr].split(',').collect do |s|
-            s.match(/'[\s\w]+'/) ? $MATCH.strip[1..-2] : subj.locate(add_name_space_prefix s.strip).first
-          end
-        else
-          subj.locate(add_name_space_prefix xform[attr]).first
-        end
-      end
-    end.compact
-    children = xform.nodes.collect do |child|
-      activate(child, subj)
-    end
-    args << children.flatten if children.any?
-    args
-  end
-
-  def add_name_space_prefix(str)
-    str.split('/').collect do |w|
-      w.match(/\w+/) ? "#{src_ns ? src_ns+':' : ''}#{w}" : w
-    end.join('/')
   end
 end
--- test/con_duxml_test.rb
+++ test/con_duxml_test.rb
@@ -7,8 +7,20 @@ include ConDuxml
 SAMPLE_FILE = File.expand_path(File.dirname(__FILE__) + '/../xml/dma.xml')
 SIMPLE_TREE = File.expand_path(File.dirname(__FILE__) + '/../xml/transforms/simple_tree.xml')
 SIMPLE_TREE_OUT = File.expand_path(File.dirname(__FILE__) + '/../xml/transforms/answers/simple_tree.dita')
+OBJ_METH = File.expand_path(File.dirname(__FILE__) + '/../xml/transforms/object_method.xml')
+OBJ_METH_OUT = File.expand_path(File.dirname(__FILE__) + '/../xml/transforms/answers/object_method.dita')
+NESTED_METH = File.expand_path(File.dirname(__FILE__) + '/../xml/transforms/nested_methods.xml')
+NESTED_METH_OUT = File.expand_path(File.dirname(__FILE__) + '/../xml/transforms/answers/nested_methods.dita')
+GET = File.expand_path(File.dirname(__FILE__) + '/../xml/transforms/get.xml')
+GET_OUT = File.expand_path(File.dirname(__FILE__) + '/../xml/transforms/answers/get.dita')
+
 class ConDuxmlTest < Test::Unit::TestCase
-  include Duxml
+  include Dita
+
+  def boo(arg)
+    "<p>#{@source.nodes.first.text}, boo! Hi #{arg}</p>"
+  end
+
   def setup
 
   end
@@ -20,6 +32,31 @@ class ConDuxmlTest < Test::Unit::TestCase
     assert_not_same doc, t
 
     # saving output for demo purposes
-    save 'simple_tree.dita', t
+    #save SIMPLE_TREE_OUT, t
+  end
+
+  def test_transform_plugin_method
+    t = transform OBJ_METH, SAMPLE_FILE
+    load OBJ_METH_OUT
+    assert_equal doc.root[0].to_s, t.root[0].to_s
+    assert_not_same doc, t
+
+    #save OBJ_METH_OUT, t
+  end
+
+  def test_nested_methods
+    t = transform NESTED_METH, SAMPLE_FILE
+    #save NESTED_METH_OUT, t
+    load NESTED_METH_OUT
+    assert_equal doc.root[0].to_s, t.root[0].to_s
+    assert_not_same doc, t
+  end
+
+  def test_get
+    t = transform GET, SAMPLE_FILE
+    #save GET_OUT, t
+    load GET_OUT
+    assert_equal doc.root[0].to_s, t.root[0].to_s
+    assert_not_same doc, t
   end
 end
